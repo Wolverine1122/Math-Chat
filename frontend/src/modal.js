53db5b7e7e0bd1e3ebcd34fd729b9715851650a4
vnikonov63 <nikonov.va@yandex.ru>
2021-03-14 16:44:57 +0400

    some progress

--- frontend/src/modal.js
+++ frontend/src/modal.js
@@ -6,17 +6,42 @@ const modalStyles = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   backgroundColor: "#FFF",
-  padding: "50px",
+  paddingTop: "20px",
+  paddingBottom: "20px",
   zIndex: 2,
 };
 
-const outterStyles = {};
+const outterStyles = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: "rgba(0, 0, 0, .7)",
+  zIndex: 2,
+};
 
 function Modal({ open, onClose }) {
   return open ? (
-    <div style={modalStyles}>
-      <button onClick={onClose}>Close</button>
-    </div>
+    <>
+      <div style={outterStyles}></div>
+      <div style={modalStyles}>
+        <div class="row">
+          <div class="col s1">
+            <i class="material-icons prefix" onClick={onClose}>
+              close
+            </i>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s8 offset-s1">
+            <i class="material-icons prefix">account_circle</i>
+            <input id="icon_prefix" type="text" class="validate" />
+            <label for="icon_prefix">Choose name</label>
+          </div>
+        </div>
+      </div>
+    </>
   ) : null;
 }
 

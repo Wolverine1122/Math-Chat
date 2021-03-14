8255516d3575ca119786a900095e699ea8e31013
Behzod <74157130+Wolverine1122@users.noreply.github.com>
2021-03-14 19:32:22 +0500

    Update courier.py

--- courier.py
+++ courier.py
@@ -4,14 +4,15 @@ client = Courier(auth_token="dk_prod_DM3TSDRZFGMA8DP389FSG1ZTDG4M")
 
 resp = client.send(
     event="R873XWW5AT4BDQQM8E416D77BQ7B",
-    recipient="Behzod",
+    recipient="user",
     profile={
       "discord": {
-        "user_id": "680619300050174023"
+        "user_id": "697456609340817499"
       }
     },
     data={
-      "favoriteAdjective": "awesomeness"
+      "username1": "Vasilii",
+      "username2": "user"
     }
 )
 
